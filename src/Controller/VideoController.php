cfbacd64ab0cfda2d70a2d74d89dc35133fc68cb
jcg678 <jcg678@hotmail.es>
2020-05-03 11:10:11 +0200

    ruta nuevo video

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -3,6 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Entity\Video;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validation;
+use Symfony\Component\Validator\Constraints\Email;
+use App\Services\JwtAuth;
 
 class VideoController extends AbstractController
 {
@@ -14,4 +22,22 @@ class VideoController extends AbstractController
             'path' => 'src/Controller/VideoController.php',
         ]);
     }
+
+    private function resjson($data){
+        $json = $this->get('serializer')->serialize($data, 'json');
+        $response = new Response();
+        $response->setContent($json);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+
+    public function create(Request $request, JwtAuth $jwtAuth){
+        $data = [
+            'status'=>'error',
+            'code'=>400,
+            'message'=>'El video no ha podido crearse'
+        ];
+        return $this->resjson($data);
+    }
+
 }
