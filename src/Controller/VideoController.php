d0a9c10f0b1011600fef75ccb5ec9dc0fe1ca3e3
jcg678 <jcg678@hotmail.es>
2020-05-03 11:31:57 +0200

    Guardando videos nuevos

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -37,6 +37,52 @@ class VideoController extends AbstractController
             'code'=>400,
             'message'=>'El video no ha podido crearse'
         ];
+
+        $token = $request->headers->get('Authorization');
+        $authCheck= $jwtAuth->checkToken($token);
+
+        if($authCheck){
+            $json = $request->get('json',null);
+            $params = json_decode($json);
+
+            $identity = $jwtAuth->checkToken($token,true);
+
+
+            if(!empty($json)){
+                $user_id = ($identity->sub != null) ? $identity->sub : null;
+                $title = (!empty($params->title)) ? $params->title : null;
+                $description = (!empty($params->description)) ? $params->description : null;
+                $url=(!empty($params->url)) ? $params->url : null;
+                if(!empty($user_id)  && !empty($title)){
+                    $em = $this->getDoctrine()->getManager();
+                    $user = $this->getDoctrine()->getRepository(User::class)->findOneBy(['id'=>$user_id]);
+
+                    $video = new Video();
+                    $video->setUser($user);
+                    $video->setTitle($title);
+                    $video->setDescription($description);
+                    $video->setUrl($url);
+                    $video->setStatus('normal');
+
+                    $dateNow = new \DateTime('now');
+                    $video->setCreatedAt($dateNow);
+                    $video->setUpdatedAt($dateNow);
+
+                    $em->persist($video);
+                    $em->flush();
+
+                    $data = [
+                        'status'=>'success',
+                        'code'=>200,
+                        'message'=>'El video se ha guardado',
+                        'video' => $video
+                    ];
+
+                }
+            }
+        }
+
+
         return $this->resjson($data);
     }
 
